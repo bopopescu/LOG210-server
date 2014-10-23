559bc3aecfba8423f968307f679c3831dde658f9
Benjamin <benjamin@bcomeau.ca>
2014-10-23 10:19:48 -0400

    modification controller dishes

--- webserver/controllers/dishes.py
+++ webserver/controllers/dishes.py
@@ -41,7 +41,7 @@ def index(id):
 
     # Check dish
     if dish is None:
-        return make_response("Le dish n'existe pas.", 400)
+        return make_response("Le plat n'existe pas.", 400)
 
     # Build the response
     response = make_response(jsonify(dish.to_dict()))
@@ -64,70 +64,24 @@ def create():
 
     # Check name
     if 'name' not in datas:
-        return make_response("Le nom du dish est obligatoire.", 400)
+        return make_response("Le nom du plat est obligatoire.", 400)
     if not isinstance(datas['name'], (str, unicode)):
         return make_response("Le nom doit etre une chaine de caractere.", 400)
 
     # Create dish
     dish = Dish(name=datas['name'])
 
-    # Check phone
-    if 'phone' in datas:
-        if not isinstance(datas['phone'], (str, unicode)):
-            return make_response("Le numero de telephone doit etre une chaine de caractere.", 400)
-        dish.phone = datas['phone']
+    # Check description
+    if 'description' in datas:
+        if not isinstance(datas['description'], (str, unicode)):
+            return make_response("La description doit etre une chaine de caractere.", 400)
+        dish.description = datas['description']
 
-    # Check cooking type
-    if 'cooking_type' in datas:
-        if not isinstance(datas['cooking_type'], (str, unicode)):
-            return make_response("Le type de cuisine doit etre une chaine de caractere.", 400)
-        dish.cooking_type = datas['cooking_type']
-
-    # Check address
-    if 'address' in datas:
-        if not isinstance(datas['address'], (str, unicode)):
-            return make_response("L'adresse doit etre une chaine de caractere.", 400)
-        dish.address = datas['address']
-
-    # Check zipcode
-    if 'zipcode' in datas:
-        if not isinstance(datas['zipcode'], (str, unicode)):
-            return make_response("Le code postal doit etre une chaine de caractere.", 400)
-        dish.zipcode = datas['zipcode']
-
-    # Check city
-    if 'city' in datas:
-        if not isinstance(datas['city'], (str, unicode)):
-            return make_response("La ville doit etre une chaine de caractere.", 400)
-        dish.city = datas['city']
-
-    # Check country
-    if 'country_id' in datas:
-        try:
-            country_id = int(datas['country_id'])
-        except Exception:  # pragma: no cover
-            return make_response("country_id doit etre un identifiant.", 400)
-
-        country = db.session.query(Country).get(country_id)
-
-        if country is None:
-            return make_response("Le pays n\'existe pas.", 404)
-
-        dish.country = country
-
-    # Check restaurateur
-    if 'restaurateur_id' in datas:
-        try:
-            restaurateur_id = int(datas['restaurateur_id'])
-        except Exception:
-            return make_response("restaurateur_id doit etre un identifiant.", 400)
-
-        restaurateur = db.session.query(Restaurateur).get(restaurateur_id)
-
-        if restaurateur is None:
-            return make_response("Le restaurateur n\'existe pas.", 404)
-
-        dish.restaurateur = restaurateur
+    # Check price
+    if 'price' in datas:
+        if not isinstance(datas['price'], (float, unicode)):
+            return make_response("Le prix doit etre numerique", 400)
+        dish.price = datas['price']
 
     # Add dish
     db.session.add(dish)
@@ -137,7 +91,7 @@ def create():
         db.session.commit()
     except Exception:  # pragma: no cover
         db.session.rollback()
-        return make_response("Due a une erreur inconnu, le restaurateur ne peut pas etre cree.", 500)
+        return make_response("Due a une erreur inconnu, le plat ne peut pas etre cree.", 500)
 
     # Build the response
     response = make_response(jsonify(dish.to_dict()))
@@ -170,74 +124,26 @@ def update(id):
 
         dish.name = datas['name']
 
-    # Check city
-    if 'city' in datas:
-        if not isinstance(datas['city'], (str, unicode)):
-            return make_response("La ville doit etre une chaine de caractere.", 400)
-        dish.city = datas['city']
+    # Check description
+    if 'description' in datas:
+        if not isinstance(datas['description'], (str, unicode)):
+            return make_response("La description doit etre une chaine de caractere.", 400)
+        dish.description = datas['description']
 
-    # Check address
-    if 'address' in datas:
-        if not isinstance(datas['address'], (str, unicode)):
-            return make_response("L'adresse doit etre une chaine de caractere.", 400)
+    # Check price
+    if 'price' in datas:
+        if not isinstance(datas['price'], (float, unicode)):
+            return make_response("Le prix doit etre numerique.", 400)
 
-        dish.address = datas['address']
+        dish.price = datas['price']
 
-    # Check phone
-    if 'phone' in datas:
-        if not isinstance(datas['phone'], (str, unicode)):
-            return make_response("Le numero de telephone doit etre une chaine de caractere.", 400)
-
-        dish.phone = datas['phone']
-
-    # Check zipcode
-    if 'zipcode' in datas:
-        if not isinstance(datas['zipcode'], (str, unicode)):
-            return make_response("Le code postal doit etre une chaine de caractere.", 400)
-        dish.zipcode = datas['zipcode']
-
-    # Check country
-    if 'country_id' in datas:
-        try:
-            country_id = int(datas['country_id'])
-        except Exception:  # pragma: no cover
-            return make_response("country_id doit etre un identifiant.", 400)
-
-        country = db.session.query(Country).get(country_id)
-
-        if country is None:
-            return make_response("Le pays n\'existe pas.", 404)
-
-        dish.country = country
-    else:
-        dish.country = None
-
-    # Check restaurateur
-    if 'restaurateur_id' in datas:
-        try:
-            restaurateur_id = int(datas['restaurateur_id'])
-        except Exception:  # pragma: no cover
-            return make_response("restaurateur_id doit etre un identifiant.", 400)
-
-        restaurateur = db.session.query(Restaurateur).get(restaurateur_id)
-
-        if restaurateur is None:
-            return make_response("Le restaurateur n\'existe pas.", 404)
-
-        if restaurateur.dish is not None:
-            if restaurateur.dish.id != dish.id:
-                return make_response("Le restaurateur est deja assignee a un dish.", 400)
-
-        dish.restaurateur = restaurateur
-    else:
-        dish.restaurateur = None
 
     # Commit
     try:
         db.session.commit()
     except Exception:  # pragma: no cover
         db.session.rollback()
-        return make_response("Due a une erreur inconnu, le dish ne peut pas etre modifie.", 500)
+        return make_response("Due a une erreur inconnu, le plat ne peut pas etre modifie.", 500)
 
     # Build the response
     response = make_response(jsonify(dish.to_dict()))
@@ -261,10 +167,10 @@ def delete(id):
 
     # Check dish
     if dish is None:
-        return make_response("Le dish n'existe pas.", 404)
+        return make_response("Le plat n'existe pas.", 404)
 
-    # Unlink restaurateur from dish
-    dish.restaurateur = None
+    # Unlink menu from dish
+    #dish.menu = None
 
     # Delete dish
     db.session.delete(dish)
@@ -274,7 +180,7 @@ def delete(id):
         db.session.commit()
     except Exception:  # pragma: no cover
         db.session.rollback()
-        return make_response("Due a une erreur inconnu, le dish ne peut pas etre supprime.", 500)
+        return make_response("Due a une erreur inconnu, le plat ne peut pas etre supprime.", 500)
 
     # Build the response
     response = make_response(jsonify(dish.to_dict()))
